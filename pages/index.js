a9d2c41c1091666db47e7efad6e81c599ac8df3e
PRobertoGSA <131317547+PRobertoGSA@users.noreply.github.com>
2025-02-19 12:50:01 +0000

    atualizando

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>neste momento você sentiu o verdadeiro AMOR parte 2 </h1>
+    return <h1>neste momento você sentiu o verdadeiro AMOR parte 3 </h1>
 }
 
 export default Home;
