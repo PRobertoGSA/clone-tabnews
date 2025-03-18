5016bb73fbcb9d2111ab2583d2f8cd6e9293bfa6
PRobertoGSA <131317547+PRobertoGSA@users.noreply.github.com>
2025-03-18 13:32:37 +0000

    atualização da mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>neste momento você sentiu o verdadeiro AMOR cell?</h1>
+    return <h1>Eu te amo, se você me ama da uma risadinha
+        
+    </h1>
 }
 
 export default Home;
