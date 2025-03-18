4ab30a36cf8fdb69f709e33d31942903a146e7a4
PRobertoGSA <131317547+PRobertoGSA@users.noreply.github.com>
2025-03-18 14:18:43 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Treinamento de um projeto do curso
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 function Home() {
-    return <h1>Eu te amo, se você me ama grave um video do seu sorisso
-    </h1>
+  return <h1>Eu te amo, se você me ama grave um video do seu sorisso</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
 export default Home;
